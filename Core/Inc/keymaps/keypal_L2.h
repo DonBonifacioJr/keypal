b4603c643fa13b06723eea2017276113ba506a45
Adam Mariano <77022624+HRTlessness@users.noreply.github.com>
2025-04-21 19:53:05 -0400

    Working with USB
    Usb is sending reports and generating button presses, GPIO and I2C are initialized in .IOC. If USB stop working and gives a "driver error" try overwriting the current driver and middleware folders again

---
+++ Core/Inc/keymaps/keypal_L2.h
@@ -0,0 +1,22 @@
+/*
+ * keypal_L2.h
+ *
+ *  Created on: Apr 21, 2025
+ *      Author: adamj
+ */
+
+#ifndef INC_KEYMAPS_KEYPAL_L2_H_
+#define INC_KEYMAPS_KEYPAL_L2_H_
+
+uint16_t KEY_MAP[4][3] = {
+		/*	KP1		KP2		KP3	*/
+			{0x59,	0x5A,	0x5B},
+		/*	KP4		KP5		KP6	*/
+			{0x5C,	0x5D,	0x5E},
+		/*	KP7		KP8		KP9	*/
+			{0x5F,	0x60,	0x61},
+		/*			KP0		NMLK*/
+			{0x00,	0x62,	0x53}
+};
+
+#endif /* INC_KEYMAPS_KEYPAL_L2_H_ */
